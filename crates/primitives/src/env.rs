7df913e219586c2b29aa85b0458d3971fe99847a
Brecht Devos <Brechtp.Devos@gmail.com>
2025-02-12 00:32:39 +0100

    add yolo account abstraction

--- crates/primitives/src/env.rs
+++ crates/primitives/src/env.rs
@@ -232,14 +232,14 @@ impl Env {
         // EIP-3607: Reject transactions from senders with deployed code
         // This EIP is introduced after london but there was no collision in past
         // so we can leave it enabled always
-        if !self.cfg.is_eip3607_disabled() {
-            let bytecode = &account.info.code.as_ref().unwrap();
-            // allow EOAs whose code is a valid delegation designation,
-            // i.e. 0xef0100 || address, to continue to originate transactions.
-            if !bytecode.is_empty() && !bytecode.is_eip7702() {
-                return Err(InvalidTransaction::RejectCallerWithCode);
-            }
-        }
+        // if !self.cfg.is_eip3607_disabled() {
+        //     let bytecode = &account.info.code.as_ref().unwrap();
+        //     // allow EOAs whose code is a valid delegation designation,
+        //     // i.e. 0xef0100 || address, to continue to originate transactions.
+        //     if !bytecode.is_empty() && !bytecode.is_eip7702() {
+        //         return Err(InvalidTransaction::RejectCallerWithCode);
+        //     }
+        // }
 
         // Check that the transaction's nonce is correct
         if let Some(tx) = self.tx.nonce {
